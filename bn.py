9b2f7e720bcd018ad14b59b49dee597258e5d4a6
root <root@monitor.sz.lan>
2021-06-02 19:22:36 +0800

    minor update

--- bn.py
+++ bn.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
 
-import datetime
+import argparse
+import datetime as dt
 import os
 import socket
 import subprocess
-import sys
 import time
 
-boot_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# This delay has to be long enough to accommodate the startup time of pfSense.
-time.sleep(300)
+def main():
 
-fsck_info = ''
-if os.uname()[4][:3] == 'arm':
-    fsck_info += '\n\nIt appears that this is an ARM-based platform, its fsck information is shown as follows:\n'
-    for line in open('/var/log/syslog'):
-        if "systemd-fsck" in line:
-            fsck_info += line
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--debug', dest='debug', action='store_true')
+    args = vars(ap.parse_args())
+    debug_mode = args['debug']
 
-ps = subprocess.Popen(('/bin/echo', '-e', 'Subject:[{}] Booted\n{} is booted at {}{}'.format(socket.gethostname(), socket.gethostname(), boot_time, fsck_info)), stdout=subprocess.PIPE)
-output = subprocess.check_output(('/usr/bin/msmtp', '-t', 'root'), stdin=ps.stdout)
+    boot_time = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    if debug_mode is False:
+        time.sleep(300)
+        # Delay the message so that it is sent AFTER network is up.
+
+    fsck_info = ''
+    if os.uname()[4][:3] == 'arm':
+        fsck_info += '\n\nIt appears that this is an ARM-based platform, '
+        fsck_info += 'its fsck information is shown as follows:\n'
+        for line in open('/var/log/syslog'):
+            if "systemd-fsck" in line:
+                fsck_info += line
+
+    command = ('/bin/echo', '-e',
+               (f'Subject:Device [{socket.gethostname()}] booted\n'
+                f'{socket.gethostname()} is booted at {boot_time}{fsck_info}'))
+    ps = subprocess.Popen(command, stdout=subprocess.PIPE)
+    output = subprocess.check_output(('/usr/bin/msmtp', '-t', 'root'),
+                                     stdin=ps.stdout)
 
 
+if __name__ == '__main__':
+
+    main()
